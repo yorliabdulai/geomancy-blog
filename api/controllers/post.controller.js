f423c8ffb2b5ada37b99ac3d8e24e1a4c311440c
yorliabdulai <yorliabdulai1@gmail.com>
2024-10-13 17:18:35 +0000

    refactor: Remove authentication requirement for creating posts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from '../utils/error.js';
 export const create = async (req, res, next) => {
   try {
     // Check if the user exists and is an admin
-    if (!req.user || !req.user.isAdmin) {
+    if (!req.user) {
       return res.status(403).json({ message: 'You are not allowed to create a post' });
     }
 
