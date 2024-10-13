32f5ccd521e792c4578d76a8795b20581a10cde4
yorliabdulai <yorliabdulai1@gmail.com>
2024-10-13 17:03:22 +0000

    refactor: Remove authentication requirement for creating posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,7 +4,7 @@ import { create, deletepost, getposts, updatepost } from '../controllers/post.co
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create)
+router.post('/create',  create)
 router.get('/getposts', getposts)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
