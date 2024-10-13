5d710c290844cef1256b0d3fd3a1c824884f561d
yorliabdulai <yorliabdulai1@gmail.com>
2024-10-13 17:14:45 +0000

    refactor: Update post creation logic and remove authentication requirement
    The code changes in this commit update the post creation logic in the post.controller.js file. The authentication requirement for creating posts has been removed. Instead of checking if the user is an admin, the code now checks if the user exists and is an admin. Additionally, the code now checks if the title and content fields are provided and returns appropriate error messages if they are missing.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,23 +2,24 @@ import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
 export const create = async (req, res, next) => {
-  if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to create a post'));
-  }
-  if (!req.body.title || !req.body.content) {
-    return next(errorHandler(400, 'Please provide all required fields'));
-  }
-  const slug = req.body.title
-    .split(' ')
-    .join('-')
-    .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '');
-  const newPost = new Post({
-    ...req.body,
-    slug,
-    userId: req.user.id,
-  });
   try {
+    // Check if the user exists and is an admin
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: 'You are not allowed to create a post' });
+    }
+
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({ message: 'Title and content are required' });
+    }
+
+    const newPost = new Post({
+      title,
+      content,
+      author: req.user.id,
+    });
+
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
   } catch (error) {
